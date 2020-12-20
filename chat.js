40e4c70d132e3fb70917da4b3ea8fa4e06c6d8a5
Gvrito123 <gvritagio@gmail.com>
2020-12-20 23:35:50 +0400

    finished search/delete functions

--- chat.js
+++ chat.js
@@ -1,3 +1,8 @@
+const chatBody = document.getElementsByClassName('chat-content')[0];
+const msgBody = document.getElementById('messageField');
+const sendBtn = document.getElementById('sendBtn');
+const searchResults = document.getElementById('search-results');
+const searchedMsg = searchResults.getElementsByClassName('message');
 const closedChat = document.getElementById('chat-closed');
 const openedChat = document.getElementById('chat-open');
 const closeBtn = document.getElementById('x-button');
@@ -8,6 +13,10 @@ const searchShowBtn = document.getElementById('searchShow');
 const searchHideBtn = document.getElementById('searchHide');
 const searchBar = document.getElementById('searchField');
 const list = document.getElementById('dropdown-ul');
+const clearBtn = document.getElementById('clear');
+const confirm = document.getElementById('confirmation-li');
+const acceptBtn = document.getElementById('optionYes');
+const declineBtn = document.getElementById('optionNo');
 
 const openChat = () => {
     openedChat.style.animation = 'chatOpen 1s';
@@ -25,7 +34,7 @@ const closeChat = () => {
 
 const dropMenuDown = () => {
     dropMenu.style.display = 'flex';
-    dropMenu.style.animation = 'chatOpen 1s';
+    dropMenu.style.animation = 'chatOpen 0.5s';
     dropdBtn.style.display = 'none';
     dropuBtn.style.display = 'inline';
 }
@@ -40,21 +49,51 @@ const dropMenuUp = () => {
 }
 
 const showSearchField = () => {
-    searchBar.style.animation = 'chatOpen 1s';
-    list.style.animation = 'chatOpen 1s';
     searchBar.style.display = 'block';
     searchShowBtn.style.display = 'none'
     searchHideBtn.style.display = 'inline'
 }
 
 const closeSearchField = () => {
-    searchBar.style.animation = 'chatClose 0.2s';
-    list.style.animation = 'chatClose 0.2s';
-    setTimeout(() => {
-        searchBar.style.display = 'none';
-        searchHideBtn.style.display = 'none';
-        searchShowBtn.style.display = 'inline';
-    }, 150);
+    searchBar.style.display = 'none';
+    searchHideBtn.style.display = 'none';
+    searchShowBtn.style.display = 'inline';
+}
+const confirmation = () => {
+    acceptBtn.style.display = 'block';
+    declineBtn.style.display = 'block';
+    confirm.style.display = 'block';
+}
+const clearResponse = () => {
+
+}
+const confirmationHide = () => {
+    acceptBtn.style.display = 'none';
+    declineBtn.style.display = 'none';
+    confirm.style.display = 'none';
+}
+const addTag = (target,tag,value,id,classname,br) => {
+    // if(typeof target != 'string') throw new Error('target tag is required and should be a string');
+    if(typeof tag != 'string') throw new Error('tag is required and should be a string');
+    if(typeof value == 'undefined') throw new Error('value is required');
+    let newTag = document.createElement(`${tag}`);
+    let newInner = document.createTextNode(`${value}`);
+    newTag.appendChild(newInner);
+    if(typeof id != 'undefined') {
+        if(typeof id != 'string') throw new Error('id should be a string');
+        newTag.id = `${id}`;
+    }
+    if(typeof classname != 'undefined') {
+        // if(typeof classname != 'string') throw new Error('class should be a string');
+        newTag.className = `${classname}`;
+    }
+    target.appendChild(newTag);
+    if(typeof br != 'undefined') {
+        if(typeof br != 'boolean') throw new Error('br should be a boolean');
+        let breakLine = document.createElement('br');
+        target.appendChild(breakLine);
+    }
+    return newTag;
 }
 
 dropdBtn.addEventListener('click',dropMenuDown);
@@ -63,4 +102,16 @@ closedChat.addEventListener('click',openChat);
 closeBtn.addEventListener('click',closeChat);
 searchShowBtn.addEventListener('click',showSearchField);
 searchHideBtn.addEventListener('click',closeSearchField);
-
+clearBtn.addEventListener('click',() => {
+    confirmation();
+    acceptBtn.onclick = () => {
+        confirmationHide();
+        dropMenuUp();
+        chatBody.innerHTML = null;
+        searchResults.innerHTML = null;
+        messages.clear();
+    }
+    declineBtn.onclick = () => {
+        confirmationHide();
+    }
+})
--- chatfunct.js
+++ chatfunct.js
@@ -1,31 +1,27 @@
-const chatBody = document.getElementsByClassName('chat-content')[0];
-const msgBody = document.getElementById('messageField');
-const sendBtn = document.getElementById('sendBtn');
-const searchResults = document.getElementById('search-results');
-const searchedMsg = searchResults.getElementsByClassName('message')
+
 
 class Messages {
-    db = new Map();
+    #db = new Map();
     lastId = 0;
     constructor(){
-
+        return true;
     };
     addMessage(msgText) {
         let id = this.lastId;
         let message = {};
         message.id = id;
         message.text = msgText
-        this.db.set(id,message);
+        this.#db.set(id,message);
         this.lastId++;
         return message.id
     }
     readMessage(id) {
-        if(!this.db.has(id)) throw new Error('Message with that id was not found')
-        return this.db.get(id);
+        if(!this.#db.has(id)) throw new Error('Message with that id was not found')
+        return this.#db.get(id);
     }
     readAll() {
         let arr = [];
-        this.db.forEach((value) => {
+        this.#db.forEach((value) => {
             arr.push(value)
         })
         return arr;
@@ -43,16 +39,14 @@ class Messages {
         else return ansArr;
     }
     sendMsg() {
-        let userTxt = msgBody.value;
-        let message = document.createElement('span');
-        let msgText = document.createTextNode(`${userTxt}`);
-        let breakLine = document.createElement('br');
-        message.appendChild(msgText);
-        message.className = 'message';
-        message.id = `message${this.addMessage(userTxt)}`
-        chatBody.appendChild(message);
-        chatBody.appendChild(breakLine);
+        let message = addTag(chatBody, 'span', msgBody.value, `message${this.addMessage(msgBody.value)}`, 'message', true);
         msgBody.value = null;
+        let coord = message.offsetTop;
+        chatBody.scrollTo(0,coord);
+    }
+    clear() {
+        this.lastId = 0;
+        return this.#db.clear();
     }
 }
 
@@ -66,27 +60,36 @@ msgBody.onkeypress = (event) => {
 }
 searchBar.onkeypress = (event) => {
     if(event.keyCode === 13) {
+        searchResults.innerHTML = null;
         let searchResult = messages.searchMsg(searchBar.value);
         if(searchResult == null) {
             searchBar.style.border = '1px solid red';
+            setTimeout(() => {
+                searchBar.style.border = '1px solid black';
+            }, 10000);
         }else {
+            searchBar.style.border = '1px solid green';
+            setTimeout(() => {
+                searchBar.style.border = '1px solid black';
+            }, 10000);
             searchResults.style.display = 'block';
             for(let value of searchResult) {
-                let msg = value.text;
-                let msgBln = document.createElement('span');
-                let msgText = document.createTextNode(`${msg}`);
-                let breakLine = document.createElement('br');
-                msgBln.appendChild(msgText);
-                msgBln.className = 'message';
-                msgBln.id = `messageFound${msg.id}`;
-                searchResults.appendChild(msgBln);
-                searchResults.appendChild(breakLine);
+                addTag(searchResults, 'span', value.text, `messageFound${value.id}`, 'message', true)
             }
-            for(let msgBox in searchedMsg) {
-                msgBox.addEventListener('click', ()=> {
-                    let origMsg = chatBody.getElementById(``)
+            searchResult.forEach((value,index) => {
+                let resultBubble = value.id;
+                let searchedMsg = document.getElementById(`messageFound${resultBubble}`);
+                let origMsg = document.getElementById(`message${resultBubble}`);
+                searchedMsg.addEventListener('click',() => {
+                    let coord = origMsg.offsetTop;
+                    dropMenuUp();
+                    chatBody.scrollTo(0,coord);
+                    origMsg.style.border = '3px solid orange';
+                    setTimeout(() => {
+                        origMsg.style.border = 'none';
+                    }, 10000);
                 })
-            }
+            })
         }
     }
 }
